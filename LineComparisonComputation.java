1fd87af197a8b3cead170cb6083ef01f1ac385bb
Praphulla Kherade <prafullkherade9@gmail.com>
2021-09-30 23:53:23 +0530

    Welcome To Line Comparison Computation Program

---
+++ LineComparisonComputation.java
@@ -0,0 +1,7 @@
+public class LineComparisonComputation{
+
+        public static void main(String args[]) {
+
+                System.out.println("Welcome To Line Comparision Program");
+        }
+}
