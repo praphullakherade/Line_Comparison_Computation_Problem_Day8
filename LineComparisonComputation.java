098bf1f76163c8f4cf606d1c54f2a2fca2e9a465
Praphulla Kherade <prafullkherade9@gmail.com>
2021-09-30 23:58:15 +0530

    Length of line

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -3,5 +3,9 @@ public class LineComparisonComputation{
         public static void main(String args[]) {
 
                 System.out.println("Welcome To Line Comparision Program");
+                System.out.println("Welcome To Line Comparision Program");
+                int X1=6,Y1=2,X2=2,Y2=8;
+                double lengthOfLine = Math.sqrt((X2-X1)*(X2-X1) + (Y2-Y1) *(Y2-Y1)) ;
+                System.out.println("Length of line "+"("+X1+","+Y1+"),"+"("+X2+","+Y2+") is "+lengthOfLine);
         }
 }
