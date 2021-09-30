6f178537f786f57f2e9b3f1e63414a087bb44f1c
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-01 00:04:12 +0530

    Checking two lines are equal or not

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -3,9 +3,14 @@ public class LineComparisonComputation{
         public static void main(String args[]) {
 
                 System.out.println("Welcome To Line Comparision Program");
-                System.out.println("Welcome To Line Comparision Program");
                 int X1=6,Y1=2,X2=2,Y2=8;
                 double lengthOfLine = Math.sqrt((X2-X1)*(X2-X1) + (Y2-Y1) *(Y2-Y1)) ;
                 System.out.println("Length of line "+"("+X1+","+Y1+"),"+"("+X2+","+Y2+") is "+lengthOfLine);
+
+                int X3=12,Y3=4,X4=4,Y4=16;
+                double lengthOfLine1 = Math.sqrt((X4-X3)*(X4-X3) + (Y4-Y3) *(Y4-Y3)) ;
+                System.out.println("Length of Line "+"("+X3+","+Y3+"),"+"("+X4+","+Y4+") is "+lengthOfLine1);
+                
+                System.out.println( lengthOfLine == lengthOfLine1);//false
         }
 }
